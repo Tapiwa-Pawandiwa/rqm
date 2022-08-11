f32f8519493a385b06c47cc64bf6c3ed3fb98298
Tapiwa-Pawandiwa <tapiwataps13@gmail.com>
2022-08-11 23:57:55 +0200

    Installed GH-PAGES

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
-));  
-
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
