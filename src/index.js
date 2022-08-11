b269a56f88d6bd9b35d272705e491af4cfbbfd2e
Tapiwa-Pawandiwa <tapiwataps13@gmail.com>
2022-08-11 16:04:35 +0200

    FINAL Changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+));  
+
 
